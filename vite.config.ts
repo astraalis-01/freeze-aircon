b9db977cd43ed30e97652bd1eecc105c0dd07256
Ignis130913 <sreeram132003@gmail.com>
2026-02-08 23:38:53 +0530

    Route fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/freeze-aircon/",   // <-- add this line
+  base: "/",   // <-- change this
 
   server: {
     host: "::",
